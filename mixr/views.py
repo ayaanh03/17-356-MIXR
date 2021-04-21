c5da7d36be0c10367e95bcd0e75b42d77efb243c
Ayaan Hussain <ayaanh@cmu.edu>
2021-04-20 21:27:47 -0400

    db work

--- mixr/views.py
+++ mixr/views.py
@@ -13,13 +13,12 @@ config = {
   "appId": "1:1097388094664:web:254cf663cf38fc773d21fd",
   "measurementId": "G-V5QVSSZ5DK"
 }
+
 firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
 database=firebase.database()
 
 def mixr(request):
-    print(database)
-    day = database.child('Data').child('Day').get().val()
-    id = database.child('Data').child('Id').get().val()
-    projectname = database.child('Data').child('Projectname').get().val()
-    return render(request,"Home.html",{"day":day,"id":id,"projectname":projectname })
+    print(database.get().val())
+
+    return render(request,"Home.html",{"testval": database.get().val()})
