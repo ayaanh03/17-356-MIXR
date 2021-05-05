c866a1e173b8621e143bd9383a24ace903c6d5cd
mayavkrishnan <mvkrishn@andrew.cmu.edu>
2021-05-05 00:07:13 -0400

    changed spotify api scope back

--- mixr/views.py
+++ mixr/views.py
@@ -9,7 +9,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import os
 
-scope = 'user-top-read user-library-read playlist-modify-public'
+scope = 'user-top-read user-library-read playlist-modify-private'
 
 # os.environ["SPOTIPY_CLIENT_ID"]='3833b3fffe714b61acb9e438b90dd25a'
 # os.environ["SPOTIPY_CLIENT_SECRET"]='5f0e93275b254646b6ec8f277e2f7a6a'
@@ -92,7 +92,7 @@ def Room(request,code):
                  'spotify:track:7xS6EPi3KX8PcxuNdOPxQ5': 'Miracle - Signfield Mix'}
     results = sp.search(q=query, limit=10, offset=0, type='track', market=None)
     print(sp.current_user())
-    sp.playlist_replace_items(t['playlist_id'], context['roomSongs'].keys())
+    # sp.playlist_replace_items(t['playlist_id'], context['roomSongs'].keys())
 
     #print(sp.current_user())
     for i, item in enumerate(results['tracks']['items']):
